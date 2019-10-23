e8f31c1fa3eba184941990b780feae7c26bd8a0f
Caio Teixeira <caio.ctc.br@gmail.com>
2019-10-22 22:51:46 -0200

    Inicio CRUD

--- ProjetoCRUD/src/univs/edu/usuario/Usuario.java
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package univs.edu.usuario;
 
-/**
- *
- * @author LABORATORIO 01
- */
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
 public class Usuario {
+    
+    @Id 
+    @GeneratedValue
+    private int idUsuario;
+    
+    @Column(length = 20, nullable = false)
+    private String loguin;
+    
+    @Column(length = 20, nullable = false)
+    private String senha;
+
+    /**
+     * @return the idUsuario
+     */
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuario the idUsuario to set
+     */
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     * @return the loguin
+     */
+    public String getLoguin() {
+        return loguin;
+    }
+
+    /**
+     * @param loguin the loguin to set
+     */
+    public void setLoguin(String loguin) {
+        this.loguin = loguin;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
     
 }
